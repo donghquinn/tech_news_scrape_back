3a44f5a1e23038789523142588257a51e3c31026
Kim, Dong H <ehdgus1524@gmail.com>
2023-06-10 02:05:19 +0900

    migrate wip

--- src/main.ts
+++ src/main.ts
@@ -5,17 +5,17 @@ import helmet from 'helmet';
 
 export const bootstrap = async () => {
   const { NestFactory } = await import('@nestjs/core');
-  const source = await import('source-map-support');
+  // const source = await import('source-map-support');
   const { config } = await import('dotenv');
 
   config();
 
-  source.install();
+  // source.install();
 
   const date = new Date().toLocaleTimeString();
 
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
-    logger: ['log', 'debug', 'warn', 'error'],
+    logger: [ 'log', 'debug', 'warn', 'error' ],
   });
 
   const port = Number(process.env.APP_PORT!);
@@ -27,11 +27,11 @@ export const bootstrap = async () => {
   app.enableShutdownHooks();
 
   await app.listen(port, '0.0.0.0', () => {
-    const message = `Listening On ${port}`;
+    const message = `Listening On ${ port }`;
     const wrapper = '@'.repeat(message.length);
 
     Logger.log(wrapper);
-    Logger.log(`Scrape Manager Start: ${date}`);
+    Logger.log(`Scrape Manager Start: ${ date }`);
     Logger.log(message);
     Logger.log(wrapper);
 
