3d82cce457f4e52cebea1c27238b7109c16c1e83
dong <ehdgus1524@gmail.com>
2023-06-30 23:24:47 +0900

    wip

--- src/controllers/list/list.ctl.ts
+++ src/controllers/list/list.ctl.ts
@@ -1,6 +1,19 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Post } from "@nestjs/common";
+import { SetErrorResponse, SetResponse } from "dto/response.dto";
+import { GetList } from "libraries/providers/list.lib";
 
 @Controller("list")
 export class ScrapedList {
+    constructor(private readonly list: GetList) { }
+    
+    @Post("/get")
+    async getListController() {
+        try {
+            const dateList = await this.list.getDateList();
 
+            return new SetResponse(200, { dateList });
+        } catch (error) {
+            return new SetErrorResponse(500, { error });
+        }
+    }
 }
