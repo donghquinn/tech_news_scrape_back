e54d18a16c869297bd714a133253aa84906f3636
Kim Donghyun <kimdonghyun@Daniel-Quinns-Mac-Book.local>
2023-07-07 01:41:35 +0900

    fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,15 @@
 /* eslint-disable class-methods-use-this */
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { HeadersMiddleware } from 'middlewares/header.middleware';
+import { ListModule } from 'modules/list.module';
 import { BbcModule } from './modules/bbc.module';
 import { ClimateModule } from './modules/climate.module';
 import { HackerModule } from './modules/hacker.module';
-import { MusicModule } from './modules/music.module';
 import { NaverModule } from './modules/naver.module';
 import { PrismaModule } from './modules/prisma.module';
-import { ListModule } from 'modules/list.module';
 
 @Module({
-  imports: [ HackerModule, BbcModule, PrismaModule, MusicModule, ClimateModule, NaverModule, ListModule ],
+  imports: [ HackerModule, BbcModule, PrismaModule, ClimateModule, NaverModule, ListModule ],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
